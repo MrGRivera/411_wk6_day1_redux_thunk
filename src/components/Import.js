c557e24bae6eee1433e7d17c73f3e287a72a0fef
MrGRivera <gabrielrivera2889@gmail.com>
2020-09-15 09:15:28 -0500

    import functionality

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Container,
+} from "@material-ui/core";
 
 const Import = (props) => {
-    // fill out this component
+  return (
+    <div>
+      <Button variant="contained" color="primary" onCLick={props.fetchMakes}>
+        Import
+      </Button>
+      <Container>
+        <Table>
+          <TableBody>
+            {props.makes.map((make) => (
+              <TableRow key={make.id}>
+                <TableCell>{make.MakeName}</TableCell>
+                <TableCell>{make.MakeId}</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
 
-    return (
-        <p>Import Component</p>
-    )
-}
-
-export default Import
+export default Import;
